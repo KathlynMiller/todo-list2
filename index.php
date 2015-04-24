85043179f7b18a083f625ebad3b06f225599f0d3
Kathlyn Miller <katkit101999@hotmail.com>
2015-04-23 23:10:42 -0700

    watched video 2 and 3 coded and commented

---
+++ index.php
@@ -0,0 +1,13 @@
+index.php 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kathlyn's To-Do List</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+ <div class="warp"> <!-- adding a class -->
+
+  </div>
+</body>
+</html> 
