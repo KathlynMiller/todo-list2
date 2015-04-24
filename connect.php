ea41efac2d08bd652060beb3dbc95deb0952d9cd
Kathlyn Miller <katkit101999@hotmail.com>
2015-04-23 23:18:31 -0700

    watched video 4 coded and commented

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+//Why is this important? Why does loclahost habe to be first?
+$mysqli = new mysqli('losthost', 'root', 'root', 'tasks'); 
+//$mysqli->connect_error then we want it to die and have message
+if ($mysqli->connect_error){
+    die('Connect Error (' . $mysqli->connect_errno .')'
+    	. $mysqli->error);
+}
+else{
+	echo "Connection made"; // echo connection has been made
+}
+$mysqli->close(); // closing mysqli
+?>
