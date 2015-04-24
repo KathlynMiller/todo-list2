2bed559dcfc78d4c2fd4d3e8c2968caad076bc32
Kathlyn Miller <katkit101999@hotmail.com>
2015-04-24 00:57:02 -0700

    watched video 19 coded and commented

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <head>
 	<title>Kathlyn's To-Do List</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-  <link rel="stylesheet" type="text/css" href="css/normalize.css">
-  <link rel="stylesheet" type="text/css" href="css/reset.css">
+ 
 </head>
 <body>
   <div class="warp">
@@ -44,7 +43,7 @@
       if(new_task != ''){
         $.post('includes/add-task.php',{ task: new_task}, function(data){
            $('add-newtask input[name=new-task]'].val();
-            $(data).appendTo('task-list ul').hide().fadeIn();
+            $(data).appendTo('.task-list ul').hide().fadeIn();
          });
         }
         return false;
